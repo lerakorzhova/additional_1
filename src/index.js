5932331b3afc225a54a60e8e5dde2afd9f3a017a
lerakorzhova <lerakorzhova98@gmail.com>
2017-02-17 17:23:59 +0300

    additional-1: full solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function sum() {
-  // your solution
+module.exports = function sum(a) {
+    let res = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        res = res + arguments[i];
+    }
+    return a ? function (b, ...theArg) {
+        return b ? sum(res, b, ...theArg) : res;
+    } : res;
 }
